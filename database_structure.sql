f5e10cbf522d2b43a7aaa3d35a8213d08edf3d4f
Kayaroganam V <88090401+Kayaroganam@users.noreply.github.com>
2022-08-27 09:46:43 +0530

    Update database_structure.sql

--- database_structure.sql
+++ database_structure.sql
@@ -4,3 +4,7 @@ CREATE TABLE item_list(id INT NOT NULL AUTO_INCREMENT, item_name VARCHAR(255) NO
 CREATE TABLE selected_items(id INT NOT NULL AUTO_INCREMENT, item_id INT NOT NULL, qty FLOAT NOT NULL, price FLOAT NOT NULL, PRIMARY KEY(id));
 ALTER TABLE selected_items ADD selected_item_name VARCHAR(255) NOT NULL AFTER item_id;
 CREATE TABLE logs(id INT NOT NULL AUTO_INCREMENT, selected_item_id INT NOT NULL, selected_qty FLOAT NOT NULL,price FLOAT,date_time DATETIME, PRIMARY KEY(id));
+INSERT INTO item_list(item_name,item_price) VALUES('item2',20);
+INSERT INTO item_list(item_name,item_price) VALUES('item3',200);
+INSERT INTO item_list(item_name,item_price) VALUES('item4',184);
+INSERT INTO item_list(item_name, item_price ) VALUES('test1',10);
