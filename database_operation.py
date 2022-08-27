e9b34e8cbe8f2d0564f5a9c1cf160de5ec902683
Kayaroganam V <88090401+Kayaroganam@users.noreply.github.com>
2022-08-27 09:54:20 +0530

    Update database_operation.py

--- database_operation.py
+++ database_operation.py
@@ -20,21 +20,20 @@ except mysql.connector.Error as err:
         )
 
         mydb_cursor = item_list.cursor()
-        query1 = f'''CREATE DATABASE {var.Mysql_Database};
-        USE {var.Mysql_Database};
+        query = f'''
+        CREATE DATABASE StationeryItems;
+        USE StationeryItems;
         CREATE TABLE item_list(id INT NOT NULL AUTO_INCREMENT, item_name VARCHAR(255) NOT NULL, item_price FLOAT NOT NULL, PRIMARY KEY(id));
         CREATE TABLE selected_items(id INT NOT NULL AUTO_INCREMENT, item_id INT NOT NULL, qty FLOAT NOT NULL, price FLOAT NOT NULL, PRIMARY KEY(id));
         ALTER TABLE selected_items ADD selected_item_name VARCHAR(255) NOT NULL AFTER item_id;
         CREATE TABLE logs(id INT NOT NULL AUTO_INCREMENT, selected_item_id INT NOT NULL, selected_qty FLOAT NOT NULL,price FLOAT,date_time DATETIME, PRIMARY KEY(id));
-        '''
-        query2 = '''INSERT INTO item_list(item_name,item_price) VALUES('item1',100);
+        INSERT INTO item_list(item_name,item_price) VALUES('item1',100);
         INSERT INTO item_list(item_name,item_price) VALUES('item2',20);
         INSERT INTO item_list(item_name,item_price) VALUES('item3',200);
         INSERT INTO item_list(item_name,item_price) VALUES('item4',184);
         INSERT INTO item_list(item_name, item_price ) VALUES('test1',10);
         '''
-        mydb_cursor.execute(query1)
-        mydb_cursor.execute(query2)
+        mydb_cursor.execute(query)
         item_list.commit()
 
     else:
